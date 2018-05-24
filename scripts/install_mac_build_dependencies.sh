dbed1f5b2a6f8d75b0f00bc98602ee5cd3eb390d
Alex Brooke <akb.sbc@gmail.com>
2018-05-24 09:21:39 -0500

    Create install_mac_build_dependencies.sh

---
+++ scripts/install_mac_build_dependencies.sh
@@ -0,0 +1,14 @@
+echo =============== INSTALLING PYTHON3 ===============
+
+brew upgrade python
+
+if [ $? -ne 0 ]
+then
+  echo Failed to install python
+  exit 1
+fi
+
+echo =============== INSTALLING PIP DEPENDENCIES ===============
+
+pip3 install --upgrade pip
+pip3 install wheel cx_Freeze
